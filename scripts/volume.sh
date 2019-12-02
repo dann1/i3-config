2ba76e1a6ac7a57dcecdf4a4c7549989bc8e4f12
Daniel Clavijo Coca <dclavijo@opennebula.systems>
2019-12-02 14:10:48 -0600

    Add keybinds for BT vol control

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo $15%
-pactl set-sink-volume alsa_output.usb-Creative_Technology_Ltd_Sound_Blaster_JAM_0000000000655d7a-00.analog-stereo $15%
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.hdmi-stereo $15%
+
+# Headpones
+pactl set-sink-volume alsa_output.usb-Creative_Technology_Ltd_Sound_Blaster_JAM_0000000000655d7a-00.analog-stereo $15% i # usb
+pactl set-sink-volume bluez_sink.00_02_3C_65_5D_66.a2dp_sink $15% # bluethooth
